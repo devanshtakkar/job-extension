b443a7da174308f37964fa30d7f11a609bf9058a
Devansh Takkar <devansh@takkar.ooo>
2025-03-09 12:24:23 -0700

    first commit

---
+++ src/contentScript.ts
@@ -0,0 +1,5 @@
+console.log('Hello from content script!');
+
+let jobsElement = document.getElementById("mosaic-provider-jobcards");
+
+console.log(jobsElement);
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: false,
+    watch: {
+      include: ['src/**/*', "public/**/*"],
+      exclude: ['node_modules', 'dist', 'build', '.git']
+    },
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background.ts'),
+        contentScript: resolve(__dirname, 'src/contentScript.ts'),
+      },
+      output: {
+        entryFileNames: 'src/[name]/[name].js',
+        chunkFileNames: 'assets/js/[name].js',
+        assetFileNames: (assetInfo) => {
+          if (!assetInfo.name) return 'assets/[name][extname]';
+          
+          const info = assetInfo.name.split('.');
+          const extType = info[info.length - 1];
+          
+          if (/\.(css|scss|sass)$/.test(assetInfo.name)) {
+            return 'assets/css/[name][extname]';
+          }
+          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/.test(assetInfo.name)) {
+            return 'assets/img/[name][extname]';
+          }
+          return 'assets/[name][extname]';
+        },
+      },
+    }
+  }
+})
